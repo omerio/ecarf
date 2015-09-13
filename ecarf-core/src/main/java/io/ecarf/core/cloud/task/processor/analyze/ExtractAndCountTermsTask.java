826410de729a8c4b970403f53a3c475ea52a350b
omerio <omerio@me.com>
2015-09-13 19:19:30 +0100

    Non threaded task

--- ecarf-core/src/main/java/io/ecarf/core/cloud/task/processor/analyze/ExtractAndCountTermsTask.java
+++ ecarf-core/src/main/java/io/ecarf/core/cloud/task/processor/analyze/ExtractAndCountTermsTask.java
@@ -20,17 +20,18 @@
 
 package io.ecarf.core.cloud.task.processor.analyze;
 
-import io.ecarf.core.cloud.task.processor.ProcessLoadTask;
+import io.cloudex.framework.task.CommonTask;
+import io.cloudex.framework.utils.FileUtils;
+import io.cloudex.framework.utils.ObjectUtils;
+import io.ecarf.core.cloud.impl.google.EcarfGoogleCloudService;
+import io.ecarf.core.compress.NTripleGzipProcessor;
+import io.ecarf.core.compress.callback.ExtractTermsCallback;
 import io.ecarf.core.term.TermCounter;
 import io.ecarf.core.utils.Constants;
 import io.ecarf.core.utils.Utils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Callable;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
@@ -42,125 +43,88 @@ import com.google.common.base.Stopwatch;
  * @author Omer Dawelbeit (omerio)
  *
  */
-public class ExtractAndCountTermsTask extends ProcessLoadTask {
+public class ExtractAndCountTermsTask extends CommonTask {
 
 
     private final static Log log = LogFactory.getLog(ExtractAndCountTermsTask.class);
 
-    private Set<String> allTerms = new HashSet<String>();
-    
+    private String files;
+
     private String sourceBucket;
 
+    private String bucket;
+
+    private String schemaTermsFile;
 
     /* (non-Javadoc)
      * @see io.cloudex.framework.Executable#run()
      */
     @Override
     public void run() throws IOException {
-        log.info("Extracting all terms and counting schema terms");
-        super.run();
 
-        // upload all the extracted terms as well
-        log.info("Saving all terms to file. Number of terms: " + allTerms.size());
         Stopwatch stopwatch = Stopwatch.createStarted();
-        
-        String allTermsFile = Utils.TEMP_FOLDER + Constants.NODE_TERMS + 
-                cloudService.getInstanceId() + Constants.DOT_SER + Constants.GZIP_EXT;
-        
-        Utils.objectToFile(allTermsFile, allTerms, true);
-        
-        //String compressedFile = Utils.compressFile(allTermsFile);
-        
-        cloudService.uploadFileToCloudStorage(allTermsFile, this.getBucket());
 
-        log.info("TIMER# Serialized and uploaded terms file in: " + stopwatch);
-    }
+        log.info("START: ExtractAndCountTermsTask task" + ", memory usage: " + Utils.getMemoryUsageInGB() + "GB");
 
-    /*
-     * (non-Javadoc)
-     * @see io.ecarf.core.cloud.task.processor.ProcessLoadTask1#getSubTasks(java.util.Set)
-     */
-    @Override
-    public List<Callable<TermCounter>> getSubTasks(Set<String> files) {
-        List<Callable<TermCounter>> tasks = new ArrayList<>();
+        EcarfGoogleCloudService cloudService = (EcarfGoogleCloudService) this.getCloudService();
 
-        if(StringUtils.isBlank(sourceBucket)) {
-            log.warn("sourceBucket is empty, using bucket: " + this.getBucket());
-            this.sourceBucket = this.getBucket();
-        }
-        
-        Set<String> schemaTerms = this.getSchemaTerms();
+        TermCounter counter = null;
 
-        for(final String file: files) {
-
-            TermCounter counter = null;
-
-            if(schemaTerms != null) {
-                counter = new TermCounter();
-                counter.setTermsToCount(schemaTerms);
-            }
-
-            ExtractAndCountTermsSubTask task = 
-                    new ExtractAndCountTermsSubTask(file, this.sourceBucket, counter, this.getCloudService());
-
-            tasks.add(task);
-
+        if(StringUtils.isNoneBlank(this.schemaTermsFile)) {
+            log.info("Downloading schema terms file: " + schemaTermsFile);
+            Set<String> schemaTerms = cloudService.getSetFromCloudStorageFile(schemaTermsFile, bucket);
+            counter = new TermCounter();
+            counter.setTermsToCount(schemaTerms);
         }
 
-        return tasks;
-    }
-    
-    /*
-     * (non-Javadoc)
-     * @see io.ecarf.core.cloud.task.processor.ProcessLoadTask1#processSingleOutput(io.ecarf.core.term.TermCounter)
-     */
-    @Override
-    public void processSingleOutput(TermCounter counter) {
+        Set<String> filesSet = ObjectUtils.csvToSet(files);
+        log.info("Processing files: " + filesSet);
 
-        super.processSingleOutput(counter);
+        for(final String file: filesSet) {
 
-        // get all terms as well
+            Stopwatch stopwatch1 = Stopwatch.createStarted();
+
+            log.info("Downloading file: " + file + ", timer: 0s");
+            String localFile = Utils.TEMP_FOLDER + file;
+            cloudService.downloadObjectFromCloudStorage(file, localFile, bucket);
+
+            log.info("Processing file: " + localFile + ", timer: " + stopwatch1);
+            NTripleGzipProcessor processor = new NTripleGzipProcessor(localFile);
+            ExtractTermsCallback callback = new ExtractTermsCallback();
+            callback.setCounter(counter);
+            processor.read(callback);
+
+            Set<String> terms = callback.getResources();
+            terms.addAll(callback.getBlankNodes());
+
+            log.info("Finished processing file: " + localFile + ", memory usage: " + Utils.getMemoryUsageInGB() + "GB" + ", timer: " + stopwatch1);
+            log.info("Number of unique URIs: " + callback.getResources().size());
+            log.info("Number of blank nodes: " + callback.getBlankNodes().size());
+            log.info("Number of literals: " + callback.getLiteralCount());
+
+            String termsFile = Utils.TEMP_FOLDER + file + Constants.DOT_SER + Constants.GZIP_EXT;
+
+            Utils.objectToFile(termsFile, terms, true);
+            
+            log.info("Serialized terms file: " + termsFile + ", memory usage: " + Utils.getMemoryUsageInGB() + "GB" + ", timer: " + stopwatch1);
+
+            cloudService.uploadFileToCloudStorage(termsFile, bucket);
+
+            log.info("Uploaded terms file: " + termsFile + ", memory usage: " + Utils.getMemoryUsageInGB() + "GB" + ", timer: " + stopwatch1);
+
+        }
+
+        // write term stats to file and upload
         if(counter != null) {
-            this.allTerms = counter.getAllTerms();
-            counter.setAllTerms(null);
+
+            log.info("Saving terms stats");
+            String countStatsFile = Utils.TEMP_FOLDER + cloudService.getInstanceId() + Constants.DOT_JSON;
+            FileUtils.objectToJsonFile(countStatsFile, counter.getCount());
+            cloudService.uploadFileToCloudStorage(countStatsFile, bucket);
         }
 
-    }
+        log.info("TIMER# All files are processed successfully, elapsed time: " + stopwatch);
 
-    /*
-     * (non-Javadoc)
-     * @see io.ecarf.core.cloud.task.processor.ProcessLoadTask1#processMultiOutput(java.util.List)
-     */
-    @Override
-    public void processMultiOutput(List<TermCounter> counters) {
-
-        super.processMultiOutput(counters);
-        boolean first = true;
-        // get all terms as well
-        for(TermCounter counter: counters) {
-            if(counter != null) {
-                
-                if(first) {
-                    this.allTerms = counter.getAllTerms();
-                    first = false;
-                    
-                } else {
-                    this.allTerms.addAll(counter.getAllTerms());                    
-                }
-                
-                // aid garbage collection?
-                counter.setAllTerms(null);
-            }
-        }
-
-
-    }
-
-    /**
-     * @return the allTerms
-     */
-    public Set<String> getAllTerms() {
-        return allTerms;
     }
 
     /**
@@ -175,6 +139,48 @@ public class ExtractAndCountTermsTask extends ProcessLoadTask {
      */
     public void setSourceBucket(String sourceBucket) {
         this.sourceBucket = sourceBucket;
+    }
+
+    /**
+     * @return the files
+     */
+    public String getFiles() {
+        return files;
+    }
+
+    /**
+     * @param files the files to set
+     */
+    public void setFiles(String files) {
+        this.files = files;
+    }
+
+    /**
+     * @return the bucket
+     */
+    public String getBucket() {
+        return bucket;
+    }
+
+    /**
+     * @param bucket the bucket to set
+     */
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    /**
+     * @return the schemaTermsFile
+     */
+    public String getSchemaTermsFile() {
+        return schemaTermsFile;
+    }
+
+    /**
+     * @param schemaTermsFile the schemaTermsFile to set
+     */
+    public void setSchemaTermsFile(String schemaTermsFile) {
+        this.schemaTermsFile = schemaTermsFile;
     }
 
 }
---
+++ ecarf-core/src/main/java/io/ecarf/core/cloud/task/processor/analyze/ExtractAndCountTermsTaskOld.java
@@ -0,0 +1,180 @@
+/**
+ * The contents of this file may be used under the terms of the Apache License, Version 2.0
+ * in which case, the provisions of the Apache License Version 2.0 are applicable instead of those above.
+ *
+ * Copyright 2014, Ecarf.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package io.ecarf.core.cloud.task.processor.analyze;
+
+import io.ecarf.core.cloud.task.processor.ProcessLoadTask;
+import io.ecarf.core.term.TermCounter;
+import io.ecarf.core.utils.Constants;
+import io.ecarf.core.utils.Utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.google.common.base.Stopwatch;
+
+/**
+ * @author Omer Dawelbeit (omerio)
+ *
+ */
+public class ExtractAndCountTermsTaskOld extends ProcessLoadTask {
+
+
+    private final static Log log = LogFactory.getLog(ExtractAndCountTermsTaskOld.class);
+
+    private Set<String> allTerms = new HashSet<String>();
+    
+    private String sourceBucket;
+
+
+    /* (non-Javadoc)
+     * @see io.cloudex.framework.Executable#run()
+     */
+    @Override
+    public void run() throws IOException {
+        log.info("Extracting all terms and counting schema terms");
+        super.run();
+
+        // upload all the extracted terms as well
+        log.info("Saving all terms to file. Number of terms: " + allTerms.size());
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        
+        String allTermsFile = Utils.TEMP_FOLDER + Constants.NODE_TERMS + 
+                cloudService.getInstanceId() + Constants.DOT_SER + Constants.GZIP_EXT;
+        
+        Utils.objectToFile(allTermsFile, allTerms, true);
+        
+        //String compressedFile = Utils.compressFile(allTermsFile);
+        
+        cloudService.uploadFileToCloudStorage(allTermsFile, this.getBucket());
+
+        log.info("TIMER# Serialized and uploaded terms file in: " + stopwatch);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see io.ecarf.core.cloud.task.processor.ProcessLoadTask1#getSubTasks(java.util.Set)
+     */
+    @Override
+    public List<Callable<TermCounter>> getSubTasks(Set<String> files) {
+        List<Callable<TermCounter>> tasks = new ArrayList<>();
+
+        if(StringUtils.isBlank(sourceBucket)) {
+            log.warn("sourceBucket is empty, using bucket: " + this.getBucket());
+            this.sourceBucket = this.getBucket();
+        }
+        
+        Set<String> schemaTerms = this.getSchemaTerms();
+
+        for(final String file: files) {
+
+            TermCounter counter = null;
+
+            if(schemaTerms != null) {
+                counter = new TermCounter();
+                counter.setTermsToCount(schemaTerms);
+            }
+
+            ExtractAndCountTermsSubTask task = 
+                    new ExtractAndCountTermsSubTask(file, this.sourceBucket, counter, this.getCloudService());
+
+            tasks.add(task);
+
+        }
+
+        return tasks;
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see io.ecarf.core.cloud.task.processor.ProcessLoadTask1#processSingleOutput(io.ecarf.core.term.TermCounter)
+     */
+    @Override
+    public void processSingleOutput(TermCounter counter) {
+
+        super.processSingleOutput(counter);
+
+        // get all terms as well
+        if(counter != null) {
+            this.allTerms = counter.getAllTerms();
+            counter.setAllTerms(null);
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see io.ecarf.core.cloud.task.processor.ProcessLoadTask1#processMultiOutput(java.util.List)
+     */
+    @Override
+    public void processMultiOutput(List<TermCounter> counters) {
+
+        super.processMultiOutput(counters);
+        boolean first = true;
+        // get all terms as well
+        for(TermCounter counter: counters) {
+            if(counter != null) {
+                
+                if(first) {
+                    this.allTerms = counter.getAllTerms();
+                    first = false;
+                    
+                } else {
+                    this.allTerms.addAll(counter.getAllTerms());                    
+                }
+                
+                // aid garbage collection?
+                counter.setAllTerms(null);
+            }
+        }
+
+
+    }
+
+    /**
+     * @return the allTerms
+     */
+    public Set<String> getAllTerms() {
+        return allTerms;
+    }
+
+    /**
+     * @return the sourceBucket
+     */
+    public String getSourceBucket() {
+        return sourceBucket;
+    }
+
+    /**
+     * @param sourceBucket the sourceBucket to set
+     */
+    public void setSourceBucket(String sourceBucket) {
+        this.sourceBucket = sourceBucket;
+    }
+
+}
